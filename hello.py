2c3fc09e04160c3620347671fd884645fb234cc4
jun <sungminjunkr@gmail.com>
2021-02-08 23:01:00 +0900

    set python 3.7 on mymac :3

--- hello.py
+++ hello.py
@@ -1,1 +1,41 @@
 import this
+
+import re
+import sys
+import pprint
+
+# list comprehension
+str1 = 'foobarbaz'
+strls = [str1[i:i+2].lower() for i in range(len(str1) - 1) if re.findall('[a-z]{2}', str1[i:i+2].lower())]
+pprint.pprint(locals())
+
+
+import re
+import sys
+import pprint
+
+str1 = 'foobarbazchocobar'
+slice = 4
+search_pat = '[a-z]{{{0}}}'.format(slice)
+search_pat
+strls = [
+    str1[i:i+slice].lower() for i in range(len(str1) - (slice-1)) 
+    if re.findall(search_pat, str1[i:i+(slice)].lower())
+]
+pprint.pprint(locals())
+strls
+
+# another way
+strls = [
+    str1[i:i+2].lower() for i in range(len(str1)-1)
+    if re.findall('[a-z]{2}', str1[i:i+2].lower())
+]
+strls
+
+# mb c-like or java-like?
+strls = []
+for i in range(len(str1) -1):
+    if re.findall('[a-z]{2}', str1[i:i+2].lower()):
+        strls.append(str1[i:i+2].lower())
+strls
+
